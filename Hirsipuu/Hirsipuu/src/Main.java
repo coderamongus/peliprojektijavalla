e9972bb45606916ef3e0d3829b04856594d5c0b7
tuomaslappalainen <112608338+tuomaslappalainen@users.noreply.github.com>
2022-09-01 10:36:17 +0300

    Add files via upload

---
+++ Hirsipuu/Hirsipuu/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File Sanakirja = new File("C:/Hirsipuu/Hirsipuu/src/Sanakirja.txt");
+
+        Scanner tekstiScanner = new Scanner(Sanakirja);
+        Scanner input = new Scanner(System.in);
+
+        ArrayList<String> sanat = new ArrayList<>();
+        while (tekstiScanner.hasNextLine()) {
+            sanat.add(tekstiScanner.nextLine());
+
+        }
+
+        String arvattava = sanat.get((int) (Math.random() * sanat.size()));
+        char[] tekstiArray = arvattava.toCharArray();
+
+        char[] vastaukset = new char[tekstiArray.length];
+        for (int i = 0; i < tekstiArray.length; i++) {
+            vastaukset[i] = '?';
+        }
+
+        boolean valmis = false;
+
+        while (valmis == false) {
+            System.out.println("_______________________");
+
+            String kirjain = input.next();
+
+            while (kirjain.length() != 1 || Character.isDigit(kirjain.charAt(0))) {
+                System.out.println("Väärä syöte, Kokeile uudestaan");
+            }
+        }
+    }
+}
