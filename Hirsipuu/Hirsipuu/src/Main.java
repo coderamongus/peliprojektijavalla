147e8aa6eb18b032914fd90f6cbf40027d2e0e11
tuomaslappalainen <112608338+tuomaslappalainen@users.noreply.github.com>
2022-09-09 08:30:38 +0300

    Update Main.java

--- Hirsipuu/Hirsipuu/src/Main.java
+++ Hirsipuu/Hirsipuu/src/Main.java
@@ -60,6 +60,76 @@ public class Main {
                 }
             }
             System.out.println("\n" + "Elämiä jäljellä: " + elamat);
+            
+             if (tehty) {
+                System.out.println("Onneksi olkoon, löysit sanan!");
+                valmis = true;
+            }
+
+            if (elamat <= 0) {
+                System.out.println("Voi ei, sinä kuolit!");
+                valmis = true;
+            }
+        }
+    }
+
+    public static void Hirsipuu(int l) {
+        if (l == 6) {
+            System.out.println("|----------");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+        } else if (l == 5) {
+            System.out.println("|----------");
+            System.out.println("|    O");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+        } else if (l == 4) {
+            System.out.println("|----------");
+            System.out.println("|    O");
+            System.out.println("|    |");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+        } else if (l == 3) {
+            System.out.println("|----------");
+            System.out.println("|    O");
+            System.out.println("|   -|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+        } else if (l == 2) {
+            System.out.println("|----------");
+            System.out.println("|    O");
+            System.out.println("|   -|-");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+        } else if (l == 1) {
+            System.out.println("|----------");
+            System.out.println("|    O");
+            System.out.println("|   -|-");
+            System.out.println("|   /");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
+        } else {
+            System.out.println("|----------");
+            System.out.println("|    O");
+            System.out.println("|   -|-");
+            System.out.println("|   /|");
+            System.out.println("|");
+            System.out.println("|");
+            System.out.println("|");
         }
     }
 }
